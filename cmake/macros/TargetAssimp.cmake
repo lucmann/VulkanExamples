217f9f095271a99ab20dd369f4882c082b79809e
Luc Ma <luc@sietium.com>
2024-09-14 17:18:37 +0800

    cmake: learn about find_package
    - FOO_FOUND
    - FOO_LIBRARIES
    - FOO_LIBRARY_DIRS
    - FOO_INCLUDE_DIRS
    - FOO_LDFLAGS
    - FOO_CFLAGS
    
    Note that the it depends on FindFoo.cmake script that which cmake
    variables are non-null in fact.
    
    Signed-off-by: Luc Ma <luc@sietium.com>

--- cmake/macros/TargetAssimp.cmake
+++ cmake/macros/TargetAssimp.cmake
@@ -8,6 +8,12 @@
 
 macro(TARGET_ASSIMP)
     find_package(assimp REQUIRED)
+    message(STATUS "found: ${ASSIMP_FOUND}")
+    message(STATUS "libraries: ${ASSIMP_LIBRARIES}")
+    message(STATUS "library_dirs: ${ASSIMP_LIBRARY_DIRS}")
+    message(STATUS "include_dirs: ${ASSIMP_INCLUDE_DIRS}")
+    message(STATUS "ldflags: ${ASSIMP_LDFLAGS}")
+    message(STATUS "cflags: ${ASSIMP_CFLAGS}")
     target_include_directories(${TARGET_NAME} PUBLIC assimp::assimp)
     target_link_libraries(${TARGET_NAME} PRIVATE assimp::assimp)
 endmacro()
